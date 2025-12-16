a6b29e2e0654996071e360b74add6f69bc49f42e
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2025-12-16 18:19:30 -0300

    refactor: format TypographyVariant type for improved readability

--- src/components/DataDisplay/Typography/Typography.interface.ts
+++ src/components/DataDisplay/Typography/Typography.interface.ts
@@ -1,17 +1,17 @@
 import { HTMLAttributes } from "react";
 
-export type TypographyVariant = 
-  | 'h1' 
-  | 'h2' 
-  | 'h3' 
-  | 'h4' 
-  | 'h5' 
-  | 'h6' 
-  | 'body1' 
-  | 'body2' 
-  | 'subtitle1' 
-  | 'subtitle2' 
-  | 'caption' 
+export type TypographyVariant =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'body1'
+  | 'body2'
+  | 'subtitle1'
+  | 'subtitle2'
+  | 'caption'
   | 'overline';
 
 export type TypographyAlign = 'left' | 'center' | 'right' | 'justify';
