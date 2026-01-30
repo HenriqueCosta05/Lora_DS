4032b3c84c2a14cfe11612e170b5abd8c27cfc44
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2026-01-30 07:22:40 -0300

    Refactor code structure for improved readability and maintainability

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,12 @@
 import type { Preview } from '@storybook/nextjs-vite';
 import '../src/styles/globals.css';
-import './preview.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
